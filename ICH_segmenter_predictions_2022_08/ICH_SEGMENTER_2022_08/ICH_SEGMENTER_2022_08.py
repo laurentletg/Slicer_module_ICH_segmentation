a2124bd83cd816e611f9854b18d06e30f3aa78cc
llg <laurentletg@gmail.om>
2022-12-31 14:30:29 -0500

    fixed browse folder text that is too long pre merge with ANW

--- ICH_segmenter_predictions_2022_08/ICH_SEGMENTER_2022_08/ICH_SEGMENTER_2022_08.py
+++ ICH_segmenter_predictions_2022_08/ICH_SEGMENTER_2022_08/ICH_SEGMENTER_2022_08.py
@@ -262,7 +262,7 @@ class ICH_SEGMENTER_2022_08Widget(ScriptedLoadableModuleWidget, VTKObservationMi
 
   def updateCurrentFolder(self):
       # self.ui.CurrecntFolder.setText(os.path.join(self.CurrentFolder,self.currentCase))
-      self.ui.CurrentFolder.setText('Current folder : \n{}'.format(self.CurrentFolder))
+      self.ui.CurrentFolder.setText('Current folder : \n...{}'.format(self.CurrentFolder[-80:]))
       
   def updateCurrentPatient(self):
       self.ui.CurrentPatient.setText(f'Current case : {self.currentCase}')  
@@ -884,71 +884,71 @@ class ICH_SEGMENTER_2022_08Widget(ScriptedLoadableModuleWidget, VTKObservationMi
   #     pass
 
 
+
+#ICH_SEGMENTER_2022_08Logic
 #
-# ICH_SEGMENTER_2022_08Logic
-# #
 
-# class ICH_SEGMENTER_2022_08Logic(ScriptedLoadableModuleLogic):
-#   """This class should implement all the actual
-#   computation done by your module.  The interface
-#   should be such that other python code can import
-#   this class and make use of the functionality without
-#   requiring an instance of the Widget.
-#   Uses ScriptedLoadableModuleLogic base class, available at:
-#   https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
-#   """
+class ICH_SEGMENTER_2022_08Logic(ScriptedLoadableModuleLogic):
+  """This class should implement all the actual
+  computation done by your module.  The interface
+  should be such that other python code can import
+  this class and make use of the functionality without
+  requiring an instance of the Widget.
+  Uses ScriptedLoadableModuleLogic base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
 
-#   def __init__(self):
-#     """
-#     Called when the logic class is instantiated. Can be used for initializing member variables.
-#     """
-#     ScriptedLoadableModuleLogic.__init__(self)
+  def __init__(self):
+    """
+    Called when the logic class is instantiated. Can be used for initializing member variables.
+    """
+    ScriptedLoadableModuleLogic.__init__(self)
 
-#   def setDefaultParameters(self, parameterNode):
-#     """
-#     Initialize parameter node with default settings.
-#     """
-#     if not parameterNode.GetParameter("Threshold"):
-#       parameterNode.SetParameter("Threshold", "100.0")
-#     if not parameterNode.GetParameter("Invert"):
-#       parameterNode.SetParameter("Invert", "false")
+  def setDefaultParameters(self, parameterNode):
+    """
+    Initialize parameter node with default settings.
+    """
+    if not parameterNode.GetParameter("Threshold"):
+      parameterNode.SetParameter("Threshold", "100.0")
+    if not parameterNode.GetParameter("Invert"):
+      parameterNode.SetParameter("Invert", "false")
 
-#   def process(self, inputVolume, outputVolume, imageThreshold, invert=False, showResult=True):
-#     """
-#     Run the processing algorithm.
-#     Can be used without GUI widget.
-#     :param inputVolume: volume to be thresholded
-#     :param outputVolume: thresholding result
-#     :param imageThreshold: values above/below this threshold will be set to 0
-#     :param invert: if True then values above the threshold will be set to 0, otherwise values below are set to 0
-#     :param showResult: show output volume in slice viewers
-#     """
+  def process(self, inputVolume, outputVolume, imageThreshold, invert=False, showResult=True):
+    """
+    Run the processing algorithm.
+    Can be used without GUI widget.
+    :param inputVolume: volume to be thresholded
+    :param outputVolume: thresholding result
+    :param imageThreshold: values above/below this threshold will be set to 0
+    :param invert: if True then values above the threshold will be set to 0, otherwise values below are set to 0
+    :param showResult: show output volume in slice viewers
+    """
 
-#     if not inputVolume or not outputVolume:
-#       raise ValueError("Input or output volume is invalid")
+    if not inputVolume or not outputVolume:
+      raise ValueError("Input or output volume is invalid")
 
-#     import time
-#     startTime = time.time()
-#     logging.info('Processing started')
+    import time
+    startTime = time.time()
+    logging.info('Processing started')
 
-#     # Compute the thresholded output volume using the "Threshold Scalar Volume" CLI module
-#     cliParams = {
-#       'InputVolume': inputVolume.GetID(),
-#       'OutputVolume': outputVolume.GetID(),
-#       'ThresholdValue' : imageThreshold,
-#       'ThresholdType' : 'Above' if invert else 'Below'
-#       }
-#     cliNode = slicer.cli.run(slicer.modules.thresholdscalarvolume, None, cliParams, wait_for_completion=True, update_display=showResult)
-#     # We don't need the CLI module node anymore, remove it to not clutter the scene with it
-#     slicer.mrmlScene.RemoveNode(cliNode)
+    # Compute the thresholded output volume using the "Threshold Scalar Volume" CLI module
+    cliParams = {
+      'InputVolume': inputVolume.GetID(),
+      'OutputVolume': outputVolume.GetID(),
+      'ThresholdValue' : imageThreshold,
+      'ThresholdType' : 'Above' if invert else 'Below'
+      }
+    cliNode = slicer.cli.run(slicer.modules.thresholdscalarvolume, None, cliParams, wait_for_completion=True, update_display=showResult)
+    # We don't need the CLI module node anymore, remove it to not clutter the scene with it
+    slicer.mrmlScene.RemoveNode(cliNode)
 
-#     stopTime = time.time()
-#     logging.info(f'Processing completed in {stopTime-startTime:.2f} seconds')
+    stopTime = time.time()
+    logging.info(f'Processing completed in {stopTime-startTime:.2f} seconds')
 
 
-# #
-# ICH_SEGMENTER_2022_08Test
 #
+#ICH_SEGMENTER_2022_08Test
+
 
 class ICH_SEGMENTER_2022_08Test(ScriptedLoadableModuleTest):
   """
